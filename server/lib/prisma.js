1ae514fb715fbf5dbb8c4e97db2527bc6e97d8e6
Emvix Comercio <emvixcomercio@gmail.com>
2026-08-24 19:49:59 -0300

    deploy: af069d3

--- server/lib/prisma.js
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-export const prisma = globalThis.__prisma || new PrismaClient({ log: ['warn', 'error'] })
-if (process.env.NODE_ENV !== 'production') globalThis.__prisma = prisma
